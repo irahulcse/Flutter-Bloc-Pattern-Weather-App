6f5bb45ff09bc35aa115c4b8761396762f1400f5
Rahul Chandra <1rahulchandra1@gmail.com>
2020-06-04 13:15:21 +0530

    Automatic Commit 2020-06-04-13:15:21

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flare_flutter/flare_actor.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_bloc_pattern_weather_app/WeatherBloc.dart';
+import 'package:flutter_bloc_pattern_weather_app/WeatherModel.dart';
 import 'package:flutter_bloc_pattern_weather_app/WeatherRepo.dart';
 
 void main() => runApp(MyApp());
@@ -137,3 +138,63 @@ class SearchPage extends StatelessWidget {
     );
   }
 }
+
+
+class ShowWeather extends StatelessWidget {
+  WeatherModel weather;
+  final city;
+
+  ShowWeather(this.weather, this.city);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+          padding: EdgeInsets.only(right: 32, left: 32, top: 10),
+          child: Column(
+            children: <Widget>[
+              Text(city,style: TextStyle(color: Colors.white70, fontSize: 30, fontWeight: FontWeight.bold),),
+              SizedBox(height: 10,),
+
+              Text(weather.getTemp.round().toString()+"C",style: TextStyle(color: Colors.white70, fontSize: 50),),
+              Text("Temprature",style: TextStyle(color: Colors.white70, fontSize: 14),),
+
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Column(
+                    children: <Widget>[
+                      Text(weather.getMinTemp.round().toString()+"C",style: TextStyle(color: Colors.white70, fontSize: 30),),
+                      Text("Min Temprature",style: TextStyle(color: Colors.white70, fontSize: 14),),
+                    ],
+                  ),
+                  Column(
+                    children: <Widget>[
+                      Text(weather.getMaxTemp.round().toString()+"C",style: TextStyle(color: Colors.white70, fontSize: 30),),
+                      Text("Max Temprature",style: TextStyle(color: Colors.white70, fontSize: 14),),
+                    ],
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 20,
+              ),
+
+              Container(
+                width: double.infinity,
+                height: 50,
+                child: FlatButton(
+                  shape: new RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(10))),
+                  onPressed: (){
+                    BlocProvider.of<WeatherBloc>(context).add(ResetWeather());
+                  },
+                  color: Colors.lightBlue,
+                  child: Text("Search", style: TextStyle(color: Colors.white70, fontSize: 16),),
+
+                ),
+              )
+            ],
+          )
+      );
+  }
+}
+
