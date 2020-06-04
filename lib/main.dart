847ab640702973cd5b08b6bca8580a32e1b82c71
Rahul Chandra <1rahulchandra1@gmail.com>
2020-06-04 13:22:01 +0530

    Automatic Commit 2020-06-04-13:22:00

--- lib/main.dart
+++ lib/main.dart
@@ -63,14 +63,14 @@ class SearchPage extends StatelessWidget {
                       style: TextStyle(
                           fontSize: 40,
                           fontWeight: FontWeight.w500,
-                          color: Colors.black),
+                          color: Colors.white),
                     ),
                     Text(
                       "Instanly",
                       style: TextStyle(
                         fontSize: 40,
                         fontWeight: FontWeight.w200,
-                        color: Colors.redAccent,
+                        color: Colors.white70,
                       ),
                     ),
                     SizedBox(
@@ -81,7 +81,7 @@ class SearchPage extends StatelessWidget {
                       decoration: InputDecoration(
                         prefixIcon: Icon(
                           Icons.search,
-                          color: Colors.black87,
+                          color: Colors.white,
                         ),
                         enabledBorder: OutlineInputBorder(
                             borderRadius: BorderRadius.all(Radius.circular(10)),
@@ -93,10 +93,10 @@ class SearchPage extends StatelessWidget {
                             borderSide: BorderSide(
                                 color: Colors.blue, style: BorderStyle.solid)),
                         hintText: "City Name",
-                        hintStyle: TextStyle(color: Colors.black38),
+                        hintStyle: TextStyle(color: Colors.white38),
                       ),
                       style: TextStyle(
-                        color: Colors.black45,
+                        color: Colors.white,
                         fontWeight: FontWeight.bold,
                       ),
                     ),
@@ -139,7 +139,6 @@ class SearchPage extends StatelessWidget {
   }
 }
 
-
 class ShowWeather extends StatelessWidget {
   WeatherModel weather;
   final city;
@@ -149,52 +148,76 @@ class ShowWeather extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-          padding: EdgeInsets.only(right: 32, left: 32, top: 10),
-          child: Column(
-            children: <Widget>[
-              Text(city,style: TextStyle(color: Colors.white70, fontSize: 30, fontWeight: FontWeight.bold),),
-              SizedBox(height: 10,),
-
-              Text(weather.getTemp.round().toString()+"C",style: TextStyle(color: Colors.white70, fontSize: 50),),
-              Text("Temprature",style: TextStyle(color: Colors.white70, fontSize: 14),),
-
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  Column(
-                    children: <Widget>[
-                      Text(weather.getMinTemp.round().toString()+"C",style: TextStyle(color: Colors.white70, fontSize: 30),),
-                      Text("Min Temprature",style: TextStyle(color: Colors.white70, fontSize: 14),),
-                    ],
-                  ),
-                  Column(
-                    children: <Widget>[
-                      Text(weather.getMaxTemp.round().toString()+"C",style: TextStyle(color: Colors.white70, fontSize: 30),),
-                      Text("Max Temprature",style: TextStyle(color: Colors.white70, fontSize: 14),),
-                    ],
-                  ),
-                ],
-              ),
-              SizedBox(
-                height: 20,
-              ),
-
-              Container(
-                width: double.infinity,
-                height: 50,
-                child: FlatButton(
-                  shape: new RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(10))),
-                  onPressed: (){
-                    BlocProvider.of<WeatherBloc>(context).add(ResetWeather());
-                  },
-                  color: Colors.lightBlue,
-                  child: Text("Search", style: TextStyle(color: Colors.white70, fontSize: 16),),
-
+        padding: EdgeInsets.only(right: 32, left: 32, top: 10),
+        child: Column(
+          children: <Widget>[
+            Text(
+              city,
+              style: TextStyle(
+                  color: Colors.white70,
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Text(
+              weather.getTemp.round().toString() + "C",
+              style: TextStyle(color: Colors.white70, fontSize: 50),
+            ),
+            Text(
+              "Temprature",
+              style: TextStyle(color: Colors.white70, fontSize: 14),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Column(
+                  children: <Widget>[
+                    Text(
+                      weather.getMinTemp.round().toString() + "C",
+                      style: TextStyle(color: Colors.white70, fontSize: 30),
+                    ),
+                    Text(
+                      "Min Temprature",
+                      style: TextStyle(color: Colors.white70, fontSize: 14),
+                    ),
+                  ],
                 ),
-              )
-            ],
-          )
-      );
+                Column(
+                  children: <Widget>[
+                    Text(
+                      weather.getMaxTemp.round().toString() + "C",
+                      style: TextStyle(color: Colors.white70, fontSize: 30),
+                    ),
+                    Text(
+                      "Max Temprature",
+                      style: TextStyle(color: Colors.white70, fontSize: 14),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Container(
+              width: double.infinity,
+              height: 50,
+              child: FlatButton(
+                shape: new RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(10))),
+                onPressed: () {
+                  BlocProvider.of<WeatherBloc>(context).add(ResetWeather());
+                },
+                color: Colors.lightBlue,
+                child: Text(
+                  "Search",
+                  style: TextStyle(color: Colors.white70, fontSize: 16),
+                ),
+              ),
+            )
+          ],
+        ));
   }
 }
-
